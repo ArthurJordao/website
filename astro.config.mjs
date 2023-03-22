cefafa7125d8f99bd0d9ce6f00fdc4492376bcd8
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2023-03-22 16:59:20 -0300

    yeah a lot of things were done and I will not add a useful description (again)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
+  output: 'server',
+  integrations: [
+    mdx(),
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
+      apiOptions: {
+        region: 'us'
+      }
+    })],
 });
